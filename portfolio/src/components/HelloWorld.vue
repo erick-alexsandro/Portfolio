017056ba03f083b78822acc852ced452f0ec3c44
Erick Alexsandro <111057605+erick-alexsandro@users.noreply.github.com>
2025-01-14 00:23:21 -0300

    feat: initial commit with basic setup on the Vue template

---
+++ portfolio/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script>
+</script>
+
+<template>
+  <div class="greetings">
+    <h2 class="green">hey! i'm <bold class="name green">Eric Alex</bold>👋</h2>
+    <h3>
+      i’m a brazilian developer with skills ranging from building beautiful websites
+      to creating desktop applications.
+    </h3>
+    <div class="icons">
+      <a href="https://github.com/erick-alexsandro" target="_blank" rel="noopener"><font-awesome-icon icon="fa-solid fa-square-envelope" /></a>
+      <a href="https://github.com/erick-alexsandro" target="_blank" rel="noopener"><font-awesome-icon icon="fa-brands fa-square-github" /></a>
+      <a href="https://www.linkedin.com/in/erick-alexsandro/" target="_blank" rel="noopener"><font-awesome-icon icon="fa-brands fa-linkedin" /></a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+.name {
+  font-weight: 700;
+  text-decoration: underline;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.icons {
+  display: flex;
+  gap: 2px;
+  font-size: 2rem;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ portfolio/src/components/TheWelcome.vue
@@ -0,0 +1,39 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>PomodoroApp</template>
+
+    PomodoroApp is an application that helps users manage their tasks and study sessions
+    using the Pomodoro technique. It includes functionalities for creating, editing and completing tasks, as well as controlling work time and breaks.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>YelpCamp</template>
+    This project empowers users to explore and review campsites from around the world.
+    The platform features a user-friendly interface, star-based reviews, user registration,
+    image uploading, and an interactive map displaying clusters of campsites.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>ChatGotch</template>
+    ChatGotch is a Discord bot that allows users to create, edit, and delete their own pets.
+    These pets can fight other pets, ask for food, and, if not fed, will eventually die.
+  </WelcomeItem>
+
+</template>
